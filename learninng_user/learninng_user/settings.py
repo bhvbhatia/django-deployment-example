d66276e6c965c88a5fd27e51290f40c196b274f5
bhvbhatia <64572038+bhvbhatia@users.noreply.github.com>
2020-04-30 11:49:51 +0530

    Update settings.py

--- learninng_user/learninng_user/settings.py
+++ learninng_user/learninng_user/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '82p332=zj!d%c889b^*b!fsztd8y3mduxwlltd#%*eqx+0=sdi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bhvbhatia.pythonanywhere.com']
 
 
 # Application definition
